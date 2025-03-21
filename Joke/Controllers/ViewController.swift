0234254d275e1f22bb2898dec9c1d028e858ecd2
Daniil Isakov <serverpgig@yandex.ru>
2025-03-21 14:32:40 +0300

    Added IBOutlets and IBActions

--- Joke/Controllers/ViewController.swift
+++ Joke/Controllers/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     //MARK: - Outlets
-    
+    @IBOutlet var showButton: UIButton!
+    @IBOutlet var refreshButton: UIButton!
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var questionLabel: UILabel!
     
     //MARK: - Private Properties
     
     
+    //MARK: - Models
     
 
     //MARK: - Life Cycles
@@ -24,7 +28,11 @@ class ViewController: UIViewController {
     
     
     //MARK: - Actions
+    @IBAction func refreshButtonClicked(_ sender: UIButton) {
+    }
     
+    @IBAction func showButtonClicked(_ sender: UIButton) {
+    }
     
     //MARK: - Private Methods
     
