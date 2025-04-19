9d9be2701233ce3de5f66af6fa6d90e811b84942
Daniil Isakov <serverpgig@yandex.ru>
2025-04-19 22:43:59 +0300

    fix(RandomJoke): changed id type to Int to match API response

--- Joke/Models/RandomJoke.swift
+++ Joke/Models/RandomJoke.swift
@@ -3,7 +3,7 @@ import Foundation
 struct RandomJoke: Codable {
     let question: String
     let punchline: String
-    let id: String
+    let id: Int
     
     private enum CodingKeys: String, CodingKey {
         case question = "setup"
